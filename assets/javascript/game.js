ac30202953f9666f7db8d3fb8e1b690581b33004
BonesTheGhost <22923497+BonesTheGhost@users.noreply.github.com>
2019-08-10 10:11:17 -0500

    Hooked up the external stylesheets and .js and verified they are working properly. Also established the basics of the game.js structure (computerChoices Array)

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+/*Setting up the array for the computer choices and testing to make sure it works properly*/
+console.log("I'm working!");
+
+var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+console.log("This is the data Array: ", computerChoices);
+
+for( i = 0; i < computerChoices.length; i++){
+    console.log(computerChoices[i]);
+    }
+
